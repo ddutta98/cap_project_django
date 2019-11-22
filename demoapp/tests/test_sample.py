a8b61db8d6277a4fc908f829b6b5a82a291d5a6a
ddutta98 <32028770+ddutta98@users.noreply.github.com>
2019-11-22 09:39:32 +0530

    Add files via upload

---
+++ demoapp/tests/test_sample.py
@@ -0,0 +1,11 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+import json
+
+class TestViews(TestCase):
+    def test_view(self):
+        client = Client()
+        response = client.get(reverse('index'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response,'index.html')
+
